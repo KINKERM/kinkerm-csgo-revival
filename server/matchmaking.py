bdb32b451e54e19f5a2dda8f149a3e5166fab368
KINKERM <mkroblox25@gmail.com>
2026-09-25 20:57:55 +0200

    matchmaking: clean direct UDP readiness path

--- server/matchmaking.py
+++ server/matchmaking.py
@@ -347,7 +347,6 @@ class MatchmakingCoordinator:
 
             ready_match_id = int(body.get("ready_match_id") or 0)
             native_reservation_id = int(body.get("reservation_id") or 0)
-            game_server_id = int(body.get("server_id") or 0)
             if ready_match_id and native_reservation_id:
                 self._server["ready_match_id"] = ready_match_id
                 match = self._matches.get(ready_match_id)
