adc89cd571611a35fc3f2e88f182d6ea602543b6
KINKERM <mkroblox25@gmail.com>
2026-09-25 22:47:26 +0200

    client gc: dedupe match rewards by match id

--- csgo_gc-patch/gc_client.h
+++ csgo_gc-patch/gc_client.h
@@ -34,7 +34,6 @@ private:
     void MatchmakingHello(GCMessageRead &messageRead);
     void PollMatchmakingBridge();
     void PollRewardBridge();
-    void ProcessBridgeMatchEnd(const std::unordered_map<std::string, std::string> &state);
     void SendMatchmakingConnectReserve();
     void ClientRequestNewMission(GCMessageRead &messageRead);
     void ClientRedeemMissionReward(GCMessageRead &messageRead);
@@ -76,7 +75,7 @@ private:
     uint32_t m_matchmakingClientVersion{};
     uint64_t m_lastMatchmakingReservation{};
     uint64_t m_lastRewardedReservation{};
-    uint64_t m_lastBridgeRewardedMatch{};
+    uint64_t m_lastRewardedMatchId{};
     uint32_t m_matchmakingIdleTicks{};
     uint64_t m_matchmakingServerId{};
     uint32_t m_matchmakingDirectUdpIp{};
